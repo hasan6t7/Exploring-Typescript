5f23038506ff018f3fbbb462a94c9d40a0e8daee
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2025-12-22 14:11:52 +0600

    array and tuples

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,22 @@ function greet(name) {
     return `Hello, ${name}`;
 }
 console.log(greet("Typescript"));
+// primitive data type
+// string
+let studentName = "Xyz";
+studentName = "Hasanujjaman";
+// number
+let age = 21;
+age = 25;
+// boolean
+let isMarried = true;
+isMarried = false;
+// Array and tuples
+// Array
+let score = [90, 85, 97];
+let score2 = [95, 85, 97];
+console.log(score2[0]);
+// Tuples
+let student = ["Hasan", 21, false];
+console.log(student[0]);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,16 @@ age = 25;
 
 let isMarried: boolean = true;
 isMarried = false;
+
+// Array and tuples
+
+// Array
+
+let score: number[] = [90, 85, 97];
+let score2: Array<number> = [95, 85, 97];
+console.log(score2[0]);
+
+// Tuples
+
+let student: [string, number, boolean] = ["Hasan", 21, false];
+console.log(student[0]);
