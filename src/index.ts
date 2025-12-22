71fbe7dffc2371c10d9948d0139ddaabf4208d49
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2025-12-22 14:00:58 +0600

    primitive data type

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,20 @@ function greet(name: string): string {
   return `Hello, ${name}`;
 }
 console.log(greet("Typescript"));
+
+// primitive data type
+
+// string
+
+let studentName: string = "Xyz";
+studentName = "Hasanujjaman";
+
+// number
+
+let age: number = 21;
+age = 25;
+
+// boolean
+
+let isMarried: boolean = true;
+isMarried = false;
