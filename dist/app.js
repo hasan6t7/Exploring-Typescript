fe912d32f0721981dd36351503f67ae5fd1189d3
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-01 14:56:10 +0600

    implecit return

--- dist/app.js
+++ dist/app.js
@@ -4,5 +4,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function throwError() {
     throw new Error("Something Went Wrong!!");
 }
-throwError();
+// throwError();
+// arrow function
+const add = (a, b) => {
+    return a + b;
+};
+console.log(add(33, 33));
+const add2 = (x, y) => x + y;
+console.log(add2(66, 66));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,15 @@ function throwError(): never {
   throw new Error("Something Went Wrong!!");
 }
 
-throwError();
+// throwError();
+
+// arrow function
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(add(33, 33));
+
+const add2 = (x: number, y: number) => x + y;  //implicit return
+console.log(add2(66,66))
